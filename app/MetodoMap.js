80db396a27dc186fb4254d589e7ee91ef1078b52
cardevv <113392365+cardevv@users.noreply.github.com>
2023-02-03 22:01:03 -0300

    .

---
+++ app/MetodoMap.js
@@ -0,0 +1,13 @@
+function aplicaDesconto(livros) {
+
+    const desconto = 0.3;
+
+    livrosDesconto = livros.map(livro => {
+
+        return  {...livro,preco: livro.preco - (livro.preco * desconto)} 
+    })
+
+    return livrosDesconto
+
+
+}
---
+++ app/forEach.js
@@ -0,0 +1,26 @@
+const InserirLivros = document.querySelector('#livros')
+
+function exibirLivros (listaLivros) {
+
+    listaLivros.forEach(livro =>{
+        InserirLivros.innerHTML += `
+
+
+        <div class="livro">
+      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+      ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+                    
+        
+        
+        `
+
+    })
+
+}
